8af62432ae49e2a5d0669348f1f39abe9c8a542d
Alfonso de los Rios <alfonso@lookatmobile.com>
2018-09-28 16:11:59 -0700

    Command line working

--- predict.py
+++ predict.py
@@ -19,74 +19,91 @@ from train import sequential_model
 # Arguments invoked from command line
 ##
 parser = argparse.ArgumentParser()
+parser.add_argument('--checkpoint', type=str, help='From where we are getting the model')
 parser.add_argument('--top_k', type=int, help='top K most likely classes')
 parser.add_argument('--image_path', type=str, help='Image to test')
-parser.add_argument('--checkpoint', type=str, help='From where we are getting the model')
 parser.add_argument('--category_names', type=str, help='Where does model will get the categories values')
 args, _ = parser.parse_known_args()
 
-print(args)
-def predict(image_path, checkpoint = 'flower_classifier.pt', category_names='cat_to_name.json', top_k=5, gpu=False):
-    ''' Predict the class (or classes) of an image using a trained deep learning model.
-    '''
-    if args.top_k:
-      top_k = args.top_k
-    if args.checkpoint:
-      checkpoint = args.checkpoint
-    if args.category_names:
-      category_names = args.category_names
-    
-    with open(category_names, 'r') as f:
-      cat_to_name = json.load(f)
+##
+# Default Variables
+##
+checkpoint = 'flower_classifier.pt'
+category_names = 'cat_to_name.json'
+
+##
+# Replacing Values
+##
+if args.checkpoint:
+  checkpoint = args.checkpoint
+if args.category_names:
+  category_names = args.category_names
+
+##
+# Loading Category Names
+##
+with open(category_names, 'r') as f:
+  cat_to_name = json.load(f)
+
+##
+# Model Loading
+##
+model_checkpoint = torch.load(checkpoint)
+
+label_length = len(model_checkpoint['class_to_idx'])
+
+model = sequential_model(num_labels=label_length)
+
+model.load_state_dict(model_checkpoint['state_dict'])
+model.class_to_idx = model_checkpoint['class_to_idx']
+
+##
+# Process Image
+##
+def process_image(image):
+  img_loader = transforms.Compose([
+  transforms.Resize(256), 
+  transforms.CenterCrop(224), 
+  transforms.ToTensor()])
   
-    # Printing Information
-    print('-' * 20)
-    print('Image Path: {}'.format(image_path))
-    print('Loading Checkpoint: {}'.format(checkpoint))
-    print('Using GPU: {}'.format(gpu))
-    print('Top K: {}'.format(top_k))
-    print('-' * 20)
-
-    checkpoint_dict = torch.load(checkpoint)
-
-    num_labels = len(checkpoint_dict['class_to_idx'])
-    hidden_units = checkpoint_dict['hidden_units']
+  pil_image = PIL.Image.open(image)
+  pil_image = img_loader(pil_image).float()
     
-    model = sequential_model(num_labels=num_labels, hidden_units=hidden_units)
-    model.eval()
+  np_image = np.array(pil_image)    
     
-    if gpu and torch.cuda.is_available():
-      model.cuda()
+  mean_v = np.array([0.485, 0.456, 0.406])
+  std_v = np.array([0.229, 0.224, 0.225])
+  np_image = (np.transpose(np_image, (1, 2, 0)) - mean_v) / std_v    
+  np_image = np.transpose(np_image, (2, 0, 1))
+            
+  return np_image
+
+##
+# Prediction
+##
+def predict(image_path, model, top_k=5):
+  if args.top_k:
+    top_k = args.top_k
+
+  img = process_image(image_path)
   
-    img_loader = transforms.Compose([
-        transforms.Resize(256), 
-        transforms.CenterCrop(224), 
-        transforms.ToTensor()])
-    
-    pil_image = PIL.Image.open(image_path)
-    pil_image = img_loader(pil_image).float()
-    
-    np_image = np.array(pil_image)    
-    
-    mean_v = np.array([0.485, 0.456, 0.406])
-    std_v = np.array([0.229, 0.224, 0.225])
-    np_image = (np.transpose(np_image, (1, 2, 0)) - mean_v) / std_v    
-    np_image = np.transpose(np_image, (2, 0, 1))
-    
-    np_image = torch.autograd.Variable(torch.FloatTensor(np_image), requires_grad=True)
-    np_image = np_image.unsqueeze(0)
-    
-    result = model(np_image).topk(top_k)
+  image_tensor = torch.from_numpy(img).type(torch.FloatTensor)
+  model_input = image_tensor.unsqueeze(0)
+  
+  probs = torch.exp(model.forward(model_input))
+  
+  top_probs, top_labs = probs.topk(top_k)
+  top_probs = top_probs.detach().numpy().tolist()[0] 
+  top_labs = top_labs.detach().numpy().tolist()[0]
+  
+  idx_to_class = {val: key for key, val in model.class_to_idx.items()}
 
-    probs = torch.nn.functional.softmax(result[0].data, dim=1).numpy()[0]
-    classes = result[1].data.numpy()[0]
+  top_labels = [idx_to_class[lab] for lab in top_labs]
+  top_flower_labels = [cat_to_name[idx_to_class[lab]] for lab in top_labs]
 
-    print('--' * 20)
-    print('Predictions (%):', list(zip(classes, probs)))
-    for val in range(len(probs)):
-      cat_name = cat_to_name[str(classes[val])]
-      print(' - {}: {}%'.format(cat_name, probs[val] * 100))
+  return top_probs, top_labels, top_flower_labels
 
-    return probs, classes
-
-probs, classes = predict(image_path=args.image_path)
+if args.image_path:
+  image_path = args.image_path
+  probs, classes, flowers = predict(image_path=image_path, model=model)
+  print(probs, classes, flowers)
--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 # Torch Vision
 import torchvision
+import torch.optim.lr_scheduler as lr_scheduler
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from collections import OrderedDict
 
 # Torch
 import torch
@@ -10,7 +12,6 @@ import torch.nn as nn
 
 # Tools
 import numpy as np
-from collections import OrderedDict
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import argparse
@@ -21,14 +22,20 @@ import json
 # Arguments invoked from command line
 ##
 parser = argparse.ArgumentParser()
-parser.add_argument('--gpu',  action='store_true', help='Use GPU in case computer has')
 parser.add_argument('--data_dir', type=str, help='Path to dataset')
-parser.add_argument('--epochs', type=int, help='Number of epochs (Cycles)')
+parser.add_argument('--env',  type=str, help='train, test or valid')
 parser.add_argument('--arch', type=str, help='Architecture (Ready PyTorch for more info)')
+parser.add_argument('--epochs', type=int, help='Number of epochs (Cycles)')
 parser.add_argument('--learning_rate', type=float, help='Learning rate')
-parser.add_argument('--hidden_units', type=int, help='Hidden units')
-parser.add_argument('--checkpoint', type=str, help='Save trained model checkpoint to file')
+parser.add_argument('--gpu',  action='store_true', help='Use GPU in case computer has')
+parser.add_argument('--save_dir', type=str, help='Save trained model checkpoint to file')
+
 args, _ = parser.parse_known_args()
+
+env = 'train'
+
+if args.env:
+  env = args.env
 
 # Data directory
 if args.data_dir:
@@ -50,8 +57,8 @@ if args.data_dir:
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
       ]),
     'test': transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomResizedCrop(224),
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
       ]),
@@ -70,109 +77,119 @@ if args.data_dir:
   }
 
   dataloaders = {
-    'train': data.DataLoader(image_datasets['train'], batch_size=32, shuffle=True, num_workers=2),
-    'test': data.DataLoader(image_datasets['test'], batch_size=32, shuffle=True, num_workers=2),
-    'valid': data.DataLoader(image_datasets['valid'], batch_size=32, shuffle=True, num_workers=2)
+    'train': data.DataLoader(image_datasets['train'], batch_size=4, shuffle=True, num_workers=2),
+    'test': data.DataLoader(image_datasets['test'], batch_size=4, shuffle=True, num_workers=2),
+    'valid': data.DataLoader(image_datasets['valid'], batch_size=4, shuffle=True, num_workers=2)
   }
 
 ##
 # Sequential Model
 ##
-def sequential_model(arch='vgg19', num_labels=0, hidden_units=4096):
-    if arch=='vgg19':
-        model = torchvision.models.vgg19(pretrained=True)
-    if arch=='vgg16':
-        model = torchvision.models.vgg19(pretrained=True)
-    elif arch=='alexnet':
-        model = torchvision.models.alexnet(pretrained=True)
-    else:
-        print('Using VGG19 Architecture as default')
-        model = torchvision.models.vgg19(pretrained=True)
+def sequential_model(arch='vgg16', num_labels=0):
+  if arch == 'vgg19':
+    model = torchvision.models.vgg19(pretrained=True)
+  if arch == 'vgg16':
+    model = torchvision.models.vgg16(pretrained=True)
+  elif arch == 'alexnet':
+    model = torchvision.models.alexnet(pretrained=True)
+  else:
+    model = torchvision.models.vgg16(pretrained=True)
 
-    for param in model.parameters():
-        param.requires_grad = False
+  for param in model.parameters():
+    param.requires_grad = False
+  
+  features = list(model.classifier.children())[0]
+  num_filters = model.classifier[0].in_features
+  
+  features = OrderedDict([
+    ('fc1', nn.Linear(num_filters, 1000)),
+    ('relu', nn.ReLU(True)),
+    ('fc2', nn.Linear(1000, num_labels))
+  ])
 
-    model.classifier = nn.Sequential(OrderedDict([
-                          ('fc1', nn.Linear(hidden_units, 1000)),
-                          ('relu', nn.ReLU()),
-                          ('fc2', nn.Linear(1000, 102)),
-                          ('output', nn.LogSoftmax(dim=1))
-                          ]))
-    return model
-##
-# Model Training
-##
-def train_model(dataset, env='train', arch='vgg19', hidden_units=4096, epochs=25, learning_rate=0.001, gpu=False, checkpoint=''):
-  print('Starting model with ENV: {}'.format(env))
-  dataloader = dataloaders[env]
+  model.classifier = nn.Sequential(features)
 
-  # Structure declaration through Command line
+  return model
+
+
+def train_model(dataset, dataloader, arch='vgg16', env='train', hidden_units=4096, epochs=5, learning_rate=0.001, gpu=False, checkpoint=''):
+  num_labels = len(dataset.classes)
+  dataset_length = len(dataloader)
+
   if args.arch:
     arch = args.arch
-
-  if args.learning_rate:
-    learning_rate = args.learning_rate
-
-  if args.hidden_units:
-    hidden_units = args.hidden_units
-
   if args.epochs:
     epochs = args.epochs
+  if args.learning_rate:
+    learning_rate = args.learning_rate
+  if args.gpu:
+    gpu = args.gpu
+  if args.save_dir:
+    checkpoint = args.save_dir
 
-  if args.checkpoint:
-    checkpoint = args.checkpoint   
+  print('--' * 20)
+  print('Starting model with ENV: {}'.format(env))
+  print('Arch: {}'.format(arch))
+  print('Output Classes: {}'.format(num_labels))
+  print('Epochs: {}'.format(epochs))
+  print('LR: {}'.format(learning_rate))
+  print('GPU: {}'.format(gpu))
+  print('Save: {}'.format(checkpoint))
+  print('Analyzing {} per batch'.format(dataset_length))
 
-  num_labels = len(dataset.classes)
-
-  model = sequential_model(arch=arch, num_labels=num_labels, hidden_units=hidden_units)
+  model = sequential_model(arch=arch, num_labels=num_labels)
 
   criterion = nn.CrossEntropyLoss()
   optimizer = torch.optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=learning_rate, momentum=0.9)
 
-  # Use GPU if defined, otherwise use cpu
   if gpu and torch.cuda.is_available():
-      device = torch.device("cuda:0")
-      model.cuda()
+    device = torch.device("cuda:0")
+    model.cuda()
   else:
-      device = torch.device("cpu")   
+    device = torch.device("cpu")   
 
   dataset_size = len(dataset)
         
   for epoch in range(epochs):
-    print('Starting Epoch {} / {}'.format(epoch + 1, epochs))
+    print('--' * 20)
+    print('Epoch Cycle: {} / {}'.format(epoch + 1, epochs))
 
     if env == 'train':
       model.train()
     else:
       model.eval()
 
-    loss = 0.0
-    correct_answers = 0
+    running_loss = 0.0
+    running_corrects = 0
+    count = 0
 
     for inputs, labels in dataloader:
       inputs = inputs.to(device)
       labels = labels.to(device)
-          
+      count += 1
+            
       optimizer.zero_grad()
 
+      if count % 546 == 0:
+        print(count)
+
       with torch.set_grad_enabled(env == 'train'):
-        outputs = model(inputs)
+        outputs = model.forward(inputs)
         _, preds = torch.max(outputs, 1)
         loss = criterion(outputs, labels)
 
         if env == 'train':
-            loss.backward()
-            optimizer.step()
+          loss.backward()
+          optimizer.step()
 
-      loss += loss.item() * inputs.size(0)
-      correct_answers += torch.sum(preds == labels.data)
+      running_loss += loss.item() * inputs.size(0)
+      running_corrects += torch.sum(preds == labels.data)
 
-    current_loss = loss / dataset_size
-    current_accuracy = correct_answers.double() / dataset_size
+    epoch_loss = running_loss / dataset_size
+    epoch_acc = running_corrects.double() / dataset_size
 
-    print('{} Loss: {:.4f} Acc: {:.4f}'.format(env, current_loss, current_accuracy))
-
-    model.class_to_idx = image_datasets['train'].class_to_idx
+    print('{} Loss: {:.4f} Acc: {:.4f}'.format(env, epoch_loss, epoch_acc))
+    model.class_to_idx = image_datasets[env].class_to_idx
 
   if checkpoint:
     print ('Saving directory ', checkpoint) 
@@ -180,11 +197,10 @@ def train_model(dataset, env='train', arch='vgg19', hidden_units=4096, epochs=25
       'arch': arch,
       'class_to_idx': model.class_to_idx, 
       'state_dict': model.state_dict(),
-      'hidden_units': hidden_units
     }
     torch.save(checkpoint_dict, checkpoint)
 
   return model
 
 if args.data_dir:
-  train_model(image_datasets['train']) 
+  train_model(dataset = image_datasets[env], dataloader = dataloaders[env]) 
